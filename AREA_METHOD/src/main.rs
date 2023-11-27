a06db1359230eb3204c9b338c1678c2e644b22fb
Lee-Yongsu <lysu3612@gmail.com>
2023-11-27 19:19:02 +0900

    method integrated

--- AREA_METHOD/src/main.rs
+++ AREA_METHOD/src/main.rs
@@ -1,44 +1,24 @@
-// struct Object {
-//     width: u32,
-//     height: u32,
-// }
+struct Object {
+    width: u32,
+    height: u32,
+}
 
-// impl Object {
-//     fn area(&self) -> u32 {
-//         self.width * self.height
-//     }
+impl Object {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
-//     fn new(width: u32, height: u32) -> Object {
-//         Object{
-//             width,
-//             height,
-//         }
-//     }
+    fn new(width: u32, height: u32) -> Object {
+        Object{
+            width,
+            height,
+        }
+    }
 
-//     fn show(&self) {
-//         println!("{}x{} with area: {}", self.width, self.height, self.area());
-//     }
-// }
-
-// fn main() {
-//     let o = Object {
-//         width : 10,
-//         height : 15,
-//     };
-
-//     // let j = Object {
-//     //     width: 13,
-//     //     height: 11,
-//     // };
-
-//     // println!("Area is {}", o.area());
-//     // println!("Area is {}", j.area());
-
-//     let j: Object = Object::new(57, 83);
-
-//     o.show();
-//     j.show();
-// }
+    fn show(&self) {
+        println!("{}x{} with area of rectangle: {}", self.width, self.height, self.area());
+    }
+}
 
 
 struct Triangle {
@@ -59,11 +39,30 @@ impl Triangle {
     }
 
     fn show(&self) {
-        println!("Base: {}, Height: {} with area: {}", self.base, self.height, self.area());
+        println!("Base: {}, Height: {} with area of triangle: {}", self.base, self.height, self.area());
     }
 }
 
 fn main() {
+    let o = Object {
+        width : 10,
+        height : 15,
+    };
+
+    // let j = Object {
+    //     width: 13,
+    //     height: 11,
+    // };
+
+    // println!("Area is {}", o.area());
+    // println!("Area is {}", j.area());
+
+    let j: Object = Object::new(57, 83);
+
+    o.show();
+    j.show();
+
+
     let t: Triangle = Triangle::new(20, 5);
 
     t.show();
